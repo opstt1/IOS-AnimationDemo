7cbe0f791b72b5406f23b79b3124092d388738ca
lihaomiao <lihaomiao@papayamobile.cn>
2016-10-26 10:02:06 +0800

    add MCAEAGLLayer

---
+++ LHMAnimationDemo/LHMCAEAGLLayerViewController.h
@@ -0,0 +1,13 @@
+//
+//  CAEAGLLayerViewController.h
+//  LHMAnimationDemo
+//
+//  Created by LiHaomiao on 2016/10/25.
+//  Copyright © 2016年 Li Haomiao. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface LHMCAEAGLLayerViewController : BaseViewController
+
+@end
